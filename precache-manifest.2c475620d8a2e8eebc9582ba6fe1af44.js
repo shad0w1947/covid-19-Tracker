a4dd0a4eeca390c105f49746a713f51a99e436be
shad0w1947 <thakurankush947@gmail.com>
2020-04-27 17:56:44 +0530

    Updates

---
+++ precache-manifest.2c475620d8a2e8eebc9582ba6fe1af44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72fb599b47cb93a3dffdefdbc452cc9",
+    "url": "/covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "be33bf9b92673a2ec0cf",
+    "url": "/covid-19-Tracker/static/css/main.7eafed68.chunk.css"
+  },
+  {
+    "revision": "e2e1ff45fcecf91bbd75",
+    "url": "/covid-19-Tracker/static/js/2.ac74bc59.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-Tracker/static/js/2.ac74bc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be33bf9b92673a2ec0cf",
+    "url": "/covid-19-Tracker/static/js/main.df97fc57.chunk.js"
+  },
+  {
+    "revision": "9dda1c4b1fed3a5ece61",
+    "url": "/covid-19-Tracker/static/js/runtime-main.da1ab3b3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
